f397539d66bc343abc04db0e2071f9a6ebadf8a5
andri <andri@DESKTOP-FIORF47>
2020-02-04 11:46:44 +0100

    getenumerator

--- Program/Expert/RuleRepository.cs
+++ Program/Expert/RuleRepository.cs
@@ -6,7 +6,7 @@ namespace Expert
 {
     class RuleRepository
     {
-        Dictionary<string, Question> questions = new Dictionary<string, Question>();
+        List<Question> questions = new List<Question>();
         private Question question;
 
         public void addQuestion(Question question)
@@ -14,14 +14,6 @@ namespace Expert
             this.question = question;
         }
 
-        public IEnumerator<Question> getEnumerator()
-        {
-            List<Question> temp = new List<Question>();
-
-            foreach (KeyValuePair<string, Question> item in questions)
-                temp.Add(item.Value);
-
-            return new QuestionIterator(temp);
-        }
+        public IEnumerator<Question> getEnumerator() { return new QuestionIterator(questions); }
     }
 }
