f48a2610f746b7cdc564362aeb41b602ccd094e7
andri <andri@DESKTOP-FIORF47>
2020-02-04 10:00:23 +0100

    interface

--- Program/Expert/QuestionIterator.cs
+++ Program/Expert/QuestionIterator.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Expert
 {
-    class QuestionIterator
+    class QuestionIterator : IEnumerable<Question>
     {
+        IEnumerator<Question> IEnumerable<Question>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
